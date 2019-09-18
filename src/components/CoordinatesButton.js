953fc5b2f306a4ea27501cdc9d6f2c179bfe1e52
Gulnoza Muminova <gmcharmy@gmail.com>
2019-09-18 17:30:13 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,25 @@
 // Code CoordinatesButton Component Here
+import React from 'react';
+export default class CoordinatesButton extends React.Component{
+    
+
+    clickHendler = (event) => {
+        console.log(event)
+        var mouseCoordinates = [event.clientX, event.clientY]     // Get the horizontal coordinate
+        
+    //    let mouseCoordinates=[1, 2]
+    this.props.onReceiveCoordinates(mouseCoordinates)
+    // return coordinates=
+    }
+
+    render(){
+
+       return(
+           <button onClick={this.clickHendler}></button>
+// (event) => this.props.onReceiveCoordinates(clickHendler)
+       )
+
+
+    }
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,17 @@
 // Code DelayedButton Component Here
+import React from 'react';
+export default class DelayedButton extends React.Component{
+
+   clickHendler=(event)=>{
+       console.log(this.props.delay)
+       event.persist();
+    //    console.log(eventType)
+       setTimeout(()=>{this.props.onDelayedClick(event)}, this.props.delay)
+       
+   }
+    render(){
+     return( 
+     <button onClick={this.clickHendler}></button>)
+       
+    }
+}                        
